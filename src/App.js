b6714e61627c5c064d856f25f84c26a5e9f69d8a
Krushna1621 <shindekrushna825@gmail.com>
2023-06-18 16:08:33 +0530

    1 task

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
-import {  useState } from "react";
-import Cart from './Components/Cart/Cart';
-import AvailableItem from "./Components/Items/AvailableItem";
-import Header from "./Components/Layout/Header";
-import Footer from './Components/Layout/Footer';
-import classes from './App.module.css';
-import CartProvider from "./store/CartProvider";
-
+import React from 'react';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Title from './components/Title/Title';
+import Content from './components/Content/Content';
 function App() {
-  const [showCart, setShowCart] = useState(false);
-
-  const showCartHandler = () => {
-    setShowCart(true);
-  };
-
-  const hiddenCartHandler = () => {
-    setShowCart(false);
-  };
-
+ 
   return (
-    <CartProvider>
-      {showCart && <Cart onClose={hiddenCartHandler} />}
-      <Header onShowCart={showCartHandler} />
-      <main>
-        <AvailableItem />
-      </main>
-      <div className={classes['footer-container']}>
-        <Footer />
-      </div>
-    </CartProvider>
+    <div className="app">
+     <Header></Header>
+     <Title></Title>
+     <Content></Content>
+     <Footer></Footer>
+    </div>
   );
 }
 
--- src/Components/Cart/CartCloseButton.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { CgCloseR } from "react-icons/cg";
-
-const CartCloseButton = (props) => {
-  return <CgCloseR className={props.className}
-   onClick={props.onClose} />;
-};
-
-export default CartCloseButton;
